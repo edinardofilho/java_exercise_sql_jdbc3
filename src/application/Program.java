329a8a2256f5f7c7d2513b8c69357dcd185c7082
Edinardo Freitas Filho <edinardofreitas@gmail.com>
2020-08-09 00:01:46 -0300

    Projeto Inicial - Adicionando Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			
-			/*st = conn.prepareStatement(
+			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 					+ "VALUES "
@@ -32,22 +32,13 @@ public class Program {
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
-			*/
-			st = conn.prepareStatement("Insert into department (Name)"
-					+ " values ('D1'),('D2')",
-					Statement.RETURN_GENERATED_KEYS);
+						
 			int rowsAffected = st.executeUpdate();
 			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			} else {
-				System.out.println("No rows affected!");
-			}
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
 			e.printStackTrace();
 		} finally {
 			DB.closeStatement(st);
